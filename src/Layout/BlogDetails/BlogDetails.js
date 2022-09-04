b06d4b8b4ecc6af063ab8421852f72b89a2d7251
doyindev1 <ade.doyin27@gmail.com>
2022-09-04 20:38:44 +0100

    ft: story,team and blog component completed

---
+++ src/Layout/BlogDetails/BlogDetails.js
@@ -0,0 +1,42 @@
+import React from 'react'
+import classes from './BlogDetail.module.css'
+
+const BlogDetails = () => {
+    return (
+        <section className={classes.BlogDetail}>
+            <div className={classes.BlogTop}>
+                <p>Our Blog</p>
+            </div>
+            <div className={classes.BlogTop}>
+                <h1>Value proposition accelerator product management venture</h1>
+            </div>
+            <div className={classes.BlogTop}>
+                <hr />
+            </div>
+
+            <div className={classes.BlogPointsTop}>
+                <h1> We are <span>commited.</span></h1>
+                <p>Conversion angel investor entrepreneur first
+                    mover advantage. Handshake infographic mass
+                    market crowdfunding iteration. </p>
+            </div>
+            <div className={classes.BlogPointsTop}>
+                <h1> We are <span>responsible.</span></h1>
+                <p>Mass market iPhone buzz conversion analytics
+                    stock iteration responsive web design user experience
+                    business plan handshake. Return on investment seed
+                    round MVP backing supply chain.  </p>
+            </div>
+            <div className={classes.BlogPointsTop}>
+                <h1> We aim for<span>progress.</span></h1>
+                <p>Bootstrapping rockstar first mover advantage
+                    business model canvas alpha deployment launch
+                    party beta facebook metrics gamification
+                    growth hacking customer focus. </p>
+            </div>
+
+        </section>
+    )
+}
+
+export default BlogDetails
--- src/Layout/Hero/Hero.js
+++ src/Layout/Hero/Hero.js
@@ -5,31 +5,73 @@ import CustomButton from '../../components/Buttons/CustomButton'
 import bloghero from '../../assets/images/Bloghero2.png'
 const Hero = () => {
     return (
-        <section className={classes.Hero}>
-            <div className={classes.HeroTop}>
-                <div>
-                    <div className={classes.HeroLogo}>
-                        <Boldo />
-                        <h1>Boldo</h1>
+        <section>
+            <div className={classes.Hero}>
+                <div className={classes.HeroTop}>
+                    <div>
+                        <div className={classes.HeroLogo}>
+                            <Boldo />
+                            <h1>Boldo</h1>
+                        </div>
+                    </div>
+                    <div>
+                        <div className={classes.HeroLeft}>
+                            <ul className={classes.HeroNav}>
+                                <li>Product</li>
+                                <li>Services</li>
+                                <li>About</li>
+                                <li>
+                                    <CustomButton
+                                        title="Log In"
+                                        customButtonStyling={customButtonStyling}
+                                        titleStyles={titleStyles} />
+                                </li>
+                            </ul>
+                        </div>
                     </div>
                 </div>
                 <div>
-
-                    <div className={classes.HeroLeft}>
-                        <ul className={classes.HeroNav}>
-                            <li>Product</li>
-                            <li>Services</li>
-                            <li>About</li>
-                            <li><CustomButton
-                                title="Log In"
-                                customButtonStyling={customButtonStyling}
-                                titleStyles={titleStyles} /></li>
-                        </ul>
+                    <p className={classes.AboutText}>About</p>
+                    <div className={classes.AboutHeroTop}>
+                        <div>
+                            <h1 className={classes.AboutAbout}>We love to make great
+                                <br />
+                                things,
+                                things that matter.
+                            </h1>
+                        </div>
+                        <div>
+                            <p className={classes.AboutParagraph}>
+                                Funding handshake buyer business-to-business
+                                metrics iPad partnership. First mover advantage innovator
+                                success deployment non-disclosure.
+                            </p>
+                        </div>
                     </div>
                 </div>
-
             </div>
-          
+            <div className={classes.AboutHeroBottom}>
+                <p className={classes.AboutNumbers}>Our Numbers</p>
+                <div className={classes.AboutNumbersText}>
+                    <div></div>
+                    <h1>Handshake infographic mass market crowdfunding iteration.</h1>
+                    <div></div>
+                </div>
+                <div className={classes.AboutFeatures}>
+                    <div className={classes.AboutFeaturesText}>
+                        <h1>200m</h1>
+                        <p>Cool feature title</p>
+                    </div>
+                    <div className={classes.AboutFeaturesText}>
+                        <h1>10.000</h1>
+                        <p>Cool feature title</p>
+                    </div>
+                    <div className={classes.AboutFeaturesText}>
+                        <h1>240</h1>
+                        <p>Cool feature title</p>
+                    </div>
+                </div>
+            </div>
         </section>
     )
 }
